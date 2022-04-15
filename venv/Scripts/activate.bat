c9cf691ebd240c0f5af88758cfb1ca89d77abeee
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-04-15 15:20:54 +0430

    first version of Python-Flask tutorial

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Arnush\Desktop\Flask-Flaskr\venv
+set VIRTUAL_ENV=C:\Users\Arnush\Desktop\Flaskr-tutorial\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
